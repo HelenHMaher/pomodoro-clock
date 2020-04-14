0b9378876ca72e246f4aae5fa81701432644aadf
HelenHMaher <helenharlanmaher@gmail.com>
2020-04-14 17:05:40 +0200

    Updates

---
+++ precache-manifest.4cdad029c65c93d5d72e3b8667138037.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31b161983e2f25d38367f37c61e6e5d2",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "e392d88e9e642bff10a9",
+    "url": "/pomodoro-clock/static/css/main.a648c7f2.chunk.css"
+  },
+  {
+    "revision": "52e12de58318b8c40a66",
+    "url": "/pomodoro-clock/static/js/2.30301310.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-clock/static/js/2.30301310.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e392d88e9e642bff10a9",
+    "url": "/pomodoro-clock/static/js/main.161fd7a2.chunk.js"
+  },
+  {
+    "revision": "af4b36dc409d94bbb5a8",
+    "url": "/pomodoro-clock/static/js/runtime-main.4fe24094.js"
+  }
+]);
